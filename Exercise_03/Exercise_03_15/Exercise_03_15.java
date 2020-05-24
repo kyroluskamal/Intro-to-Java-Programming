6c1dcc2b69a27ffb7b4ca1ab065d8ebaaca3a74a
kyroluskamal <37916625+kyroluskamal@users.noreply.github.com>
2020-05-24 09:49:20 +0200

    Update Exercise_03_15.java

--- Exercise_03/Exercise_03_15/Exercise_03_15.java
+++ Exercise_03/Exercise_03_15/Exercise_03_15.java
@@ -13,35 +13,38 @@ import java.util.Scanner;
 
 public class Exercise_03_15 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+        // Generate a lottery number
+        int lottery = (int) (Math.random() * 1000);
+        // Prompt the user to enter a guess
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your lottery pick (Three digits): ");
+        int guess = input.nextInt();
 
-		// Generate a random three-digit number
-		int lottery = (int)(Math.random() * 1000);
+        // Get digits from lottery
+        int Ld1 = lottery / 100;
+        int Ld2 = (lottery / 10) % 10;
+        int Ld3 = lottery % 10;
 
-		// Prompt the user to enter a three-digit number
-		System.out.print("Enter a three-digit number: ");
-		int guess = input.nextInt();
+        // Get digits from guess
+        int Gd1 = guess / 100;
+        int Gd2 = (guess / 10) % 10;
+        int Gd3 = guess % 10;
+        
+        
+        System.out.println("The lottery number is " + lottery);
 
-		// Extract digits from lottery
-		int lotteryDigit1 = lottery / 100;
-		int remainingDigits = lottery % 100;
-		int lotteryDigit2 = remainingDigits / 10;
-		int lotteryDigit3 = remainingDigits % 10;
-
-		// Extract digits from guess
-		int guessDigit1 = guess / 100;
-		int remainingDigits = guess % 100;
-		int guessDigit2 = remainingDigits / 10;
-		int guessDigit3 = remainingDigits % 10;
-
-		System.out.println("The lottery number is " + lottery);
-
-		// Check the guess
-		if (guess == lottery)
-			System.out.println("Exact match: you win $10,000");
-		if (guessDigit1 == lotteryDigit2)
-		{
-
-		}
-	}
+        boolean allD = (Gd1 == Ld1 || Gd1 == Ld2 || Gd1 == Ld3) && (Gd2 == Ld1 || Gd2 == Ld2 || Gd2 == Ld3) && (Gd3 == Ld1 || Gd3 == Ld2 || Gd3 == Ld3);
+        boolean oneD = (Gd1 == Ld1 || Gd1 == Ld2 || Gd1 == Ld3) || (Gd2 == Ld1 || Gd2 == Ld2 || Gd2 == Ld3) || (Gd3 == Ld1 || Gd3 == Ld2 || Gd3 == Ld3);
+       
+        // Check the guess
+        if (guess == lottery) {
+            System.out.println("Exact match: you win $12,000");
+        } else if (allD) {
+            System.out.println("Match all digits: you win $5,000");
+        } else if (oneD) {
+            System.out.println("Match one digit: you win $2,000");
+        } else {
+            System.out.println("Sorry, no match");
+        }
+    }
 }
